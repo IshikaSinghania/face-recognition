56a144a10e57c2181bab83c66dc136f7a259f492
IshikaSinghania <59695669+IshikaSinghania@users.noreply.github.com>
2020-12-22 21:23:10 +0530

    changing response

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {res.send(db.users)})
+app.get('/', (req, res) => {res.send('it is working')})
 
 // app.post('/signin' , (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 // The above statement is similar to below statement
